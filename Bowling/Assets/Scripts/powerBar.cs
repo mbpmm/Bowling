f025ac97b231df18a221103776ef59d05463a88e
mbpmm <marianomcr92@gmail.com>
2019-04-07 21:11:15 -0300

    Sacado el script de las canaletas

--- Bowling/Assets/Scripts/powerBar.cs
+++ Bowling/Assets/Scripts/powerBar.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class powerBar : MonoBehaviour
 {
-
-    public float fullHeight = 6000.0f;
     public Slider slider;
     public GameObject bola;
     // Start is called before the first frame update
